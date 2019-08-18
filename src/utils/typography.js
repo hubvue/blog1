668db23df5bff7d8cbbaebd8934bbcfc9e259fc8
kim <kimwangchong_i@didiglobal.com>
2019-08-18 15:58:35 +0800

    style

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,8 +3,39 @@ import Wordpress2016 from "typography-theme-wordpress-2016"
 
 Wordpress2016.overrideThemeStyles = () => {
   return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
+    a: {
+      color: 'var(--textLink)',
+    },
+    hr: {
+      background: 'var(--hr)',
+    },
+    'a.gatsby-resp-image-link': {
+      boxShadow: 'none',
+    },
+    // These two are for gatsby-remark-autolink-headers:
+    'a.anchor': {
+      boxShadow: 'none',
+    },
+    'a.anchor svg[aria-hidden="true"]': {
+      stroke: 'var(--textLink)',
+    },
+    'p code': {
+      fontSize: '1rem',
+    },
+    // TODO: why tho
+    'h1 code, h2 code, h3 code, h4 code, h5 code, h6 code': {
+      fontSize: 'inherit',
+    },
+    'li code': {
+      fontSize: '1rem',
+    },
+    blockquote: {
+      color: 'inherit',
+      borderLeftColor: 'inherit',
+      opacity: '0.8',
+    },
+    'blockquote.translation': {
+      fontSize: '1em',
     },
   }
 }
