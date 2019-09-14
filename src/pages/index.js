68440e208e9e977c3b58f52efb336a640605126c
kim <kimwangchong_i@didiglobal.com>
2019-09-14 14:47:59 +0800

    [update]

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import Bio from '../components/bio'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import { rhythm } from '../utils/typography'
 
 class BlogIndex extends React.Component {
   render() {
@@ -21,17 +21,20 @@ class BlogIndex extends React.Component {
             <div key={node.fields.slug}>
               <h3
                 style={{
-                  marginBottom: rhythm(1 / 4),
+                  marginBottom: rhythm(1 / 4)
                 }}
               >
-                <Link style={{ boxShadow: `none`, color:`rgb(211,54,105)` }} to={node.fields.slug}>
+                <Link
+                  style={{ boxShadow: `none`, color: `rgb(211,54,105)` }}
+                  to={node.fields.slug}
+                >
                   {title}
                 </Link>
               </h3>
               <small>{node.frontmatter.date}</small>
               <p
                 dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
+                  __html: node.frontmatter.description || node.excerpt
                 }}
               />
             </div>
